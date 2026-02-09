db8ccf22f94d74b38620bf17e4721e595638b074
GianluigiAbbracciavento <gianluigiabbracciavento@yahoo.com>
2026-02-09 09:18:34 +0100

    titolo 404

--- src/app/_servizi_globali/animazioni_saturno/saturno-stato.service.ts
+++ src/app/_servizi_globali/animazioni_saturno/saturno-stato.service.ts
@@ -1,15 +1,34 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+ import { Injectable } from '@angular/core';
+ import { BehaviorSubject } from 'rxjs';
 
-@Injectable({ providedIn: 'root' })
-export class SaturnoStatoService {
-  saturnoPronto$ = new BehaviorSubject<boolean>(false);
+ @Injectable({ providedIn: 'root' })
+ export class SaturnoStatoService {
+   saturnoPronto$ = new BehaviorSubject<boolean>(false);
+  timerPronto: ReturnType<typeof setTimeout> | null = null;
 
-  setPronto(): void {
-    if (!this.saturnoPronto$.value) this.saturnoPronto$.next(true);
-  }
+   setPronto(): void {
+    if (this.saturnoPronto$.value) {
+      return;
+    }
 
-  reset(): void {
-    if (this.saturnoPronto$.value) this.saturnoPronto$.next(false);
-  }
-}
+    if (this.timerPronto) {
+      return;
+    }
+
+    this.timerPronto = setTimeout(() => {
+      this.saturnoPronto$.next(true);
+      this.timerPronto = null;
+    }, 50);
+   }
+
+   reset(): void {
+    if (this.timerPronto) {
+      clearTimeout(this.timerPronto);
+      this.timerPronto = null;
+    }
+
+    if (this.saturnoPronto$.value) {
+      this.saturnoPronto$.next(false);
+    }
+   }
+ }
